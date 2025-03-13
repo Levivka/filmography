a5471389b727b077d33c20c4ae5ce46c75f82b2a
Levik <123123@gmail.com>
2025-03-13 14:03:57 +0300

    docker fix and test values

--- Docker/init-db.sql
+++
@@ -1,9 +0,0 @@
--- Создание таблицы films (если она еще не существует)
-CREATE TABLE IF NOT EXISTS films (
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(255) NOT NULL,
-    director VARCHAR(255) NOT NULL,
-    year INT NOT NULL,
-    rating DOUBLE PRECISION NOT NULL,
-    synopsis TEXT
-);
